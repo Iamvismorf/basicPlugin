5279f6850d59e8e9d5a128b067e2d1913569ae5d
vismorf <96656664+Iamvismorf@users.noreply.github.com.>
2026-01-03 17:28:09 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
-#include <hyprland/src/plugins/PluginAPI.hpp>
+#define WLR_USE_UNSTABLE
 
-inline HANDLE PHANDLE = nullptr;
+#include "globals.hpp"
+
+#include <hyprland/src/Compositor.hpp>
+#include <hyprland/src/desktop/view/Window.hpp>
 
 APICALL EXPORT std::string PLUGIN_API_VERSION() { return HYPRLAND_API_VERSION; }
 
+void onNewWindow(void *, SCallbackInfo &, std::any data) {
+  auto window = std::any_cast<PHLWINDOW>(data);
+  HyprlandAPI::addNotification(PHANDLE, "hi", CHyprColor{1.0, 0.2, 0.2, 1.0},
+                               5000);
+}
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
   PHANDLE = handle;
+
+  static auto P =
+      HyprlandAPI::registerCallbackDynamic(PHANDLE, "openWindow", &onNewWindow);
 
   const std::string COMPOSITOR_HASH = __hyprland_api_get_hash();
   const std::string CLIENT_HASH = __hyprland_api_get_client_hash();
@@ -18,7 +29,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
   }
 
   return {"MyPlugin", "An amazing plugin that is going to change the world!",
-          "Me", "1.0"};
+          "Me", "1.1"};
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {}
